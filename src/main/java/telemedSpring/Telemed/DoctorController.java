1c401482daa441684c8036852566c9ff7f90ff77
mbarnjak <barnjakmartin@gmail.com>
2025-01-07 09:13:11 +0100

    Added doctor basic

--- src/main/java/telemedSpring/Telemed/DoctorController.java
+++ src/main/java/telemedSpring/Telemed/DoctorController.java
@@ -1,4 +1,23 @@
 package telemedSpring.Telemed;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DoctorController {
+
+    @GetMapping("/pacijenti")
+    public String patientList() {
+        return "patientList";
+    }
+
+    @GetMapping("/dodajPacijenta")
+    public String addPatient() {
+        return "addPatient";
+    }
+
+    @GetMapping("/zapisiPacijenta")
+    public String patientRecords() {
+        return "patientRecords";
+    }
 }
